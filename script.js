aec20bd39ba3ec984506b78206eba42511a132cc
sagar kumar <94098920+Sagar-Kumar01@users.noreply.github.com>
2025-10-28 17:50:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let container = document.querySelector(".container");
+
+	for(let i = 0; i<800; i++){
+	const newDiv = document.createElement('div');
+	newDiv.classList.add('square');
+	container.appendChild(newDiv);
+}
+
