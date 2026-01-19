6c9107fa0b9752017c22f15712e4885860fc874d
sagar kumar <94098920+Sagar-Kumar01@users.noreply.github.com>
2026-01-19 13:51:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-//your JS code here. If required.
 let container = document.querySelector(".container");
 
-	for(let i = 0; i<800; i++){
-	const newDiv = document.createElement('div');
-	newDiv.classList.add('square');
-	container.appendChild(newDiv);
-}
+for (let i = 0; i < 800; i++) {
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("square");
 
+  
+  newDiv.style.backgroundColor = "rgb(29, 29, 29)";
+
+  newDiv.addEventListener("mouseover", function () {
+    newDiv.style.backgroundColor = "green";
+  });
+
+  newDiv.addEventListener("mouseout", function () {
+    newDiv.style.backgroundColor = "rgb(29, 29, 29)";
+  });
+
+  container.appendChild(newDiv);
+}
